b9ce225604e7706c5f9d05f54d0562250c8dcec4
ak <aleshiakormendi@AK-Mac-Mini.local>
2025-11-05 21:31:35 -0700

    Added mission1-ak.R script with my message"

--- mission1-member1_ak.R
+++ mission1-member1_ak.R
@@ -1,3 +1,3 @@
 # msg of sucess run
 cat("- This script was coded by member 1 \n")
-print("Hello! Please don't judge my lack of R skills... Still learning!")
+print("Hello! Please don't judge my lack of R skills... Still learning! :)")
